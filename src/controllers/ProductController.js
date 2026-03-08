0f49007f3240e06135983329587c4b93eca40f1c
samirvithlani <samir.vithlani83955@gmail.com>
2026-03-08 12:23:32 +0530

    getproductbyid

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,16 +1,70 @@
-const productSchema = require("../models/ProductModel")
+const productSchema = require("../models/ProductModel");
 
-//api -->function 
-const getAllProducts =async(req,res)=>{
+//api -->function
+const getAllProducts = async (req, res) => {
+  //database record --> connected---> table --> products == productSchema
+  const allproducts = await productSchema.find();
+  res.json({
+    message: "products fetched..",
+    data: allproducts,
+  });
+};
 
-    //database record --> connected---> table --> products == productSchema
-    const allproducts = await productSchema.find()
+// const getProductById = async (req, res) => {
+//   //req.params.id
+//   //db.products.findOne({_id:ObjectId("")})
+//   //productSchema.findOne({_id:ObjectId(req.params.id)})
+//   //const foundProduct = await productSchema.findOne({_id:ObjecId(req.params.id)})
+//   const foundProduct = await productSchema.findById(req.params.id);
+
+//   if (foundProduct) {
+//     console.log("found produtc", foundProduct);
+//     res.json({
+//       message: "product found",
+//       data: foundProduct,
+//     });
+//   }
+//   else{
+//     res.json({
+//       message: "product not found",
+
+//     });
+//   }
+// };
+
+//with try catch
+const getProductById = async (req, res) => {
+  //req.params.id
+  //db.products.findOne({_id:ObjectId("")})
+  //productSchema.findOne({_id:ObjectId(req.params.id)})
+  //const foundProduct = await productSchema.findOne({_id:ObjecId(req.params.id)})
+
+  console.log("id", req.params.id);
+  console.log("here 1");
+  try {
+    const foundProduct = await productSchema.findById(req.params.id);
+    console.log("here 2");
+
+    if (foundProduct) {
+      console.log("found produtc", foundProduct);
+      res.json({
+        message: "product found",
+        data: foundProduct,
+      });
+    } else {
+      res.json({
+        message: "product not found",
+      });
+    }
+  } catch (err) {
     res.json({
-        message:"products fetched..",
-        data:allproducts
-    })
+      message: "error while fetching product",
+      err: err.message,
+    });
+  }
+};
 
-}
-module.exports ={
-    getAllProducts
-}
+module.exports = {
+  getAllProducts,
+  getProductById,
+};
--- src/routes/ProductRoutes.js
+++ src/routes/ProductRoutes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router()
 const productController = require("../controllers/ProductController")
 router.get("/products",productController.getAllProducts)
+router.get("/product/:id",productController.getProductById)
 module.exports = router
