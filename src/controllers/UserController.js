691db5d1f06e9d3fd6ddafa72252591d5eab4ffc
samirvithlani <samir.vithlani83955@gmail.com>
2026-04-05 11:32:58 +0530

    jwt

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,33 +1,65 @@
-const userSchema = require("../models/UserModel")
-const bcrypt = require("bcrypt")
-const mailSend = require("../utils/MailUtil")
+const userSchema = require("../models/UserModel");
+const bcrypt = require("bcrypt");
+const mailSend = require("../utils/MailUtil");
+const jwt = require("jsonwebtoken")
+const secret = "secret"
 
-const createUser = async(req,res)=>{
-
-    try{
-
-        //req.body.
-        const hashedPassword =bcrypt.hashSync(req.body.password,10)
-        //const savedUser = await userSchema.create(req.body)
-        const savedUser = await userSchema.create({...req.body,password:hashedPassword})
-        //mail...
-        var mailtest = `<html>
+const createUser = async (req, res) => {
+  try {
+    //req.body.
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+    //const savedUser = await userSchema.create(req.body)
+    const savedUser = await userSchema.create({
+      ...req.body,
+      password: hashedPassword,
+    });
+    //mail...
+    var mailtest = `<html>
                             <h1>HELLO</h1>
-                        </html>`
-        await mailSend(savedUser.email,"welcome",mailtest)
-        res.status(201).json({
-            message:"user saved Successfully",
-            data:savedUser
-        })
+                        </html>`;
+    await mailSend(savedUser.email, "welcome", mailtest);
+    res.status(201).json({
+      message: "user saved Successfully",
+      data: savedUser,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "error while creating user..",
+    });
+  }
+};
 
-    }catch(err){
+const loginUser = async (req, res) => {
+  //email,password
+  const { email, password } = req.body;
+  const foundUserFromEmail = await userSchema.findOne({ email: email }); //{...password:anycrpted..}
+  console.log(foundUserFromEmail);
+  if (foundUserFromEmail) {
 
-        res.status(500).json({
-            message:"error while creating user.."
+    if (bcrypt.compareSync(password, foundUserFromEmail.password)) {
+        //token..
+        //token generate from id only
+        const token = jwt.sign(foundUserFromEmail.toObject(),secret,{expiresIn:60})
+      res.status(200).json({
+        message:"login success",
+        //data:foundUserFromEmail
+        token:token
+      })
+    }
+    else{
+        res.status(401).json({
+            message:"invalid credentials",
         })
     }
-}
+    
+  } else {
+    res.status(404).json({
+      message: "user not found register first.",
+    });
+  }
+};
 
-module.exports={
-    createUser
-}
+module.exports = {
+  createUser,
+  loginUser,
+};
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -5,4 +5,5 @@ const requestValidator = require("../middleware/RequestValidatorMiddleware")
 const userValidationSchema = require("../validationschemas/UserValidationSchema")
 
 router.post("/",requestValidator(userValidationSchema),userController.createUser)
+router.post("/login",userController.loginUser)
 module.exports  = router
