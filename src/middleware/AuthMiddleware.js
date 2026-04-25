5bfb64bad70d5c764ec9a4e342f2f4214e1d0ea9
samirvithlani <samir.vithlani83955@gmail.com>
2026-04-25 17:15:18 +0530

    forgotpassword

--- src/middleware/AuthMiddleware.js
+++ src/middleware/AuthMiddleware.js
@@ -1,46 +1,43 @@
-const jwt = require("jsonwebtoken")
-const secret  = "secret"
+const jwt = require("jsonwebtoken");
+const secret = "secret";
+const userSchema = require("../models/UserModel");
 
-
-const authMiddleware = async(req,res,next)=>{
-
-    const token = req.headers.authorization;
-    console.log("token",token)
-    //Bearer kopkpowkopkpok
-    if(token){
-
-        //token Should be Bearer token..
-        if(token.startsWith("Bearer ")){
-            //Bearer alkasl;a;lksklsanlkasasnasksmaslkmasl;kasmasl;
-            const tokenValue = token.split(" ")[1]
-            console.log("token value",tokenValue)
-            try{
-                const decoded = await jwt.verify(tokenValue,secret)
-                console.log(decoded)
-                next()
-            }catch(err){
-                res.status(401).json({
-                    message:"invalid token",
-                    err:err
-                })
-
-            }
-
+const authMiddleware = async (req, res, next) => {
+  const token = req.headers.authorization;
+  console.log("token", token);
+  //Bearer kopkpowkopkpok
+  if (token) {
+    //token Should be Bearer token..
+    if (token.startsWith("Bearer ")) {
+      //Bearer alkasl;a;lksklsanlkasasnasksmaslkmasl;kasmasl;
+      const tokenValue = token.split(" ")[1];
+      console.log("token value", tokenValue);
+      try {
+        const decoded = await jwt.verify(tokenValue, secret);
+        console.log(decoded);
+        const foundUser = await userSchema.findById(decoded._id);
+        if (foundUser) {
+          next();
+        } else {
+          res.status(401).json({
+            message: "invalid user",
+          });
         }
-        else{
-            res.status(401).json({
-                message:"token is bot Bearer token",
-            })
-        }
-
-
-    }else{
+      } catch (err) {
         res.status(401).json({
-            message:"token is missing"
-        })
+          message: "invalid token",
+          err: err,
+        });
+      }
+    } else {
+      res.status(401).json({
+        message: "token is bot Bearer token",
+      });
     }
-
-
-
-}
-module.exports = authMiddleware
+  } else {
+    res.status(401).json({
+      message: "token is missing",
+    });
+  }
+};
+module.exports = authMiddleware;
