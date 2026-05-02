c0b7de83c150554f1896d4345c9c61ace0f4fc39
samirvithlani <samir.vithlani83955@gmail.com>
2026-05-02 17:36:47 +0530

    redis

--- app.js
+++ app.js
@@ -1,39 +1,53 @@
-require("dotenv").config() //.env file load..
-const express = require("express")
+require("dotenv").config(); //.env file load..
+const express = require("express");
 //express -->referance...
-const app = express() 
-const cron = require("node-cron")
-const dbConnection= require("./src/utils/DBConnection")
-dbConnection() //db connnection function call..
-app.use(express.json()) //allowing data type as json "ALSO"
+const app = express();
+const cron = require("node-cron");
+const dbConnection = require("./src/utils/DBConnection");
+dbConnection(); //db connnection function call..
+app.use(express.json()); //allowing data type as json "ALSO"
+const Redis = require("ioredis");
+const { Queue } = require("bullmq");
 
+const userRoutes = require("./src/routes/UserRoutes");
+app.use("/user", userRoutes);
 
-
-const userRoutes = require("./src/routes/UserRoutes")
-app.use("/user",userRoutes)
-
-const productRoutes = require("./src/routes/ProductRoutes")
+const productRoutes = require("./src/routes/ProductRoutes");
 //app.use(productRoutes)
-app.use("/product",productRoutes)
+app.use("/product", productRoutes);
 
+const categoryRoutes = require("./src/routes/CategoryRoutes");
 
-const categoryRoutes = require("./src/routes/CategoryRoutes")
-
-app.use("/category",categoryRoutes)
-
-
+app.use("/category", categoryRoutes);
 
 //server calling..
 //port variable dec
 //const PORT = 3000
 
+// cron.schedule("*/10 * * * * *",()=>{
+//     console.log("every minute...")
+// })
 
-cron.schedule("*/10 * * * * *",()=>{
-    console.log("every minute...")
+const redisConnection = new Redis({
+  host: "127.0.0.1",
+  port: 6379,
+});
+
+//queue
+
+const myQueue = new Queue("taskQueue", { connection: redisConnection });
+
+app.post("/add-job",async(req,res)=>{
+
+    const {name} = req.body;
+    await myQueue.add("task",{name},{delay:0})
+    res.json({
+        message:`task created for ${name}`
+    })
+
 })
 
-
-const PORT = process.env.PORT || 3000
-app.listen(PORT,()=>{
-    console.log(`server started on port ${PORT}`)
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
