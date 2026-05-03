d8defc7ad23cad8df53240726738b2601b6e372b
samirvithlani <samir.vithlani83955@gmail.com>
2026-05-03 11:32:27 +0530

    cache

--- app.js
+++ app.js
@@ -47,6 +47,43 @@ app.post("/add-job",async(req,res)=>{
 
 })
 
+const cacheMiddleware =async(req,res,next)=>{
+  const {userId}=  req.params; //1 //1
+  
+  try{
+    const cacheData = await redisConnection.get(userId) //1 -->**NO -->1// yes
+    if(cacheData){
+      console.log("cache hit..")
+      res.json(JSON.parse(cacheData))
+    }
+    else{
+      console.log("cache miss")
+      next()
+    }
+
+  }catch(err){
+
+    res.json(err)
+
+  }
+}
+
+const fakeData = {
+  1:{name:"jog=hn",age:23},
+  2:{name:"raj",age:23},
+  3:{name:"parth",age:23},
+  4:{name:"jau",age:23},
+  5:{name:"jeet",age:23},
+}
+
+app.get("/users/:userId",cacheMiddleware,(req,res)=>{
+
+  const {userId } = req.params //1
+  const user = fakeData[userId] //db.user.find //1 ->Yes -->string -->redis cache
+  redisConnection.setex(userId,600,JSON.stringify(user))
+  res.json(user)
+})
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
